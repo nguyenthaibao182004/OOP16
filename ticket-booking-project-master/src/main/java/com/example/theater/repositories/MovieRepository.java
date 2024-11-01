558efec34b477417290f2b470ba2e9348a6fb5be
Bick Dig <nguyenthaibao182004@gmail.com>
2024-11-01 22:35:48 +0700

    Trailer

--- ticket-booking-project-master/src/main/java/com/example/theater/repositories/MovieRepository.java
+++ ticket-booking-project-master/src/main/java/com/example/theater/repositories/MovieRepository.java
@@ -15,9 +15,11 @@ public interface MovieRepository extends JpaRepository < Movie, Long > {
             "director REGEXP CONCAT('\\\\b', :keyword, '\\\\b') OR " +
             "genre REGEXP CONCAT('\\\\b', :keyword, '\\\\b') OR " +
             "actors REGEXP CONCAT('\\\\b', :keyword, '\\\\b') OR " +
-            "language REGEXP CONCAT('\\\\b', :keyword, '\\\\b')) " +
+            "language REGEXP CONCAT('\\\\b', :keyword, '\\\\b') OR " +
+            "keyword REGEXP CONCAT('\\\\b', :keyword, '\\\\b')) " +
             "AND now_showing = :nowShowing",
             nativeQuery = true)
+
 
     List<Movie> getAllMoviesByKeyWordAndNowShowing(String keyword, boolean nowShowing);
 
